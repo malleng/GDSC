78729995d47521a010cc87f8933b8ead2d222e45
Carlos Benavides <carlosbenavides35@gmail.com>
2023-06-28 15:43:53 +0000

    feat: load test data

--- src/biobuzz/metadata.py
+++ src/biobuzz/metadata.py
@@ -9,6 +9,7 @@ class MetadataLoader:
     def __init__(self, data_dir: Path) -> None:
         self.data_dir = data_dir
         self.labels_path = self.data_dir / "labels.json"
+        self.metadata_path = self.data_dir / "metadata.csv"
         self.loaded = False
 
     def load(self) -> None:
@@ -20,12 +21,18 @@ class MetadataLoader:
             raise ValueError("Metadata has not yet been loaded")
         return list(self.labels.keys())
 
-    def get_metadata(self) -> t.Tuple[pd.Series, ...]:
+    def get_metadata(self) -> t.Tuple[pd.Series, pd.Series, pd.Series]:
         if not self.loaded:
             raise ValueError("Metadata has not yet been loaded")
-        audio_paths = list(self.data_dir.glob("**/*.wav"))
-        df = pd.DataFrame({"filename": audio_paths})
-        df["target"] = df.filename.map(lambda f: self.labels[f.name.split("_")[0]])
-        df["fold"] = df.filename.map(lambda f: f.parent.name)
-        df["filename"] = df.filename.map(lambda f: str(f))
-        return df.filename, df.target, df.fold
+        df = pd.read_csv(self.metadata_path)
+        # Replace "validation" with "val" and keep rest of the values untouched
+        df["subset"] = df.subset.map({"validation": "val"}).fillna(df.subset).astype("string")
+        df["filename"] = df.apply(
+            lambda row: self.data_dir / row.subset / row.file_name, axis=1
+        ).astype("string")
+        return df.filename, df.label, df.subset
+
+    def get_test_filenames(self) -> pd.Series:
+        """Search for test files in the data directory and return their full paths."""
+        test_files_iterator = self.data_dir.glob("test/*.wav")
+        return pd.Series(test_files_iterator).astype("string")
